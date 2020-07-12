a17a406433225d14443387ba5fb364cb7f672628
Yash-Jawale <63059729+Yash-Jawale@users.noreply.github.com>
2020-07-12 21:35:38 +0530

    Created FrontEnd Templates

---
+++ cms/templates/include/header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo htmlspecialchars( $results['pageTitle'] )?></title>
+    <link rel="stylesheet" type="text/css" href="style.css" />
+</head>
+<body>
+    <div class="container">
+        <a href=".">
+            <img id="logo" src="images/logo.jpg" alt="Widget News" />
+        </a>
